055f5d23c37f0b2ec3f30ce2e5b0bc4790db720c
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-31 10:47:03 -0700

    added post, put, delete

--- routers/actions.js
+++ routers/actions.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const actionsData = require("../data/helpers/actionModel")
+const projectData = require("../data/helpers/projectModel")
 const router = express.Router();
+
 
 router.get("/", (req, res) => {
     actionsData
@@ -26,3 +28,49 @@ router.get("/:id", (req, res) => {
 });
 
 //id, description, notes
+router.post("/", (req, res) => {
+    if(req.body.project_id && req.body.description && req.body.notes) {
+        actionsData
+            .insert(req.body)
+            .then(response => {
+                res.status(201).json({response})
+            })
+            .catch(() => {
+                res.status(500).json({error: "Failure to post action."});
+            });
+    } else {
+        res.status(404).json({error: "Required parameters are missing."});
+    }
+});
+
+router.put("/:id", (req, res) => {
+    if(req.params.id && req.body.project_id && req.body.description && req.body.notes) {
+        actionsData
+            .update(req.params.id, req.body)
+            .then(action => {
+                res.status(200).json({action})
+            })
+            .catch(() => {
+                res.status(500).json({error: "Failure to update action."});
+            });
+    } else {
+        res.status(404).json({error: "Required parameters are missing."});
+    }
+});
+
+router.delete("/:id", (req, res) => {
+    if(req.params.id){
+        actionsData
+            .remove(req.params.id)
+            .then(response => {
+                res.status(200).json({response})
+            })
+            .catch(() => {
+                res.status(500).json({error: "Failure to delete action."})
+            })
+    } else {
+        res.status(404).json({error: "Required id is missing."});
+    }
+})
+
+module.exports = router;
