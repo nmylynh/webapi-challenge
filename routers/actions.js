a585e035ec11ca4d4a9bc71b2dbace2afe2b5b90
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-05-31 10:09:16 -0700

    defined get to specific id

--- routers/actions.js
+++ routers/actions.js
@@ -6,9 +6,23 @@ router.get("/", (req, res) => {
     actionsData
         .get()
         .then(actions =>{
-            res.status(200).json(actions);
+            res.status(200).json({actions});
         })
         .catch(() => {
             res.status(500).json({ error: "Failure to get data from actions."});
         });
 });
+
+router.get("/:id", (req, res) => {
+    const {id} = req.params;
+    actionsData
+        .get(id)
+        .then(action => {
+            res.status(200).json({action})
+        })
+        .catch(() => {
+            res.status(404).json({error: "Failure to get this action."});
+        });
+});
+
+//id, description, notes
