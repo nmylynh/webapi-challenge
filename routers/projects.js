98d74024e313b172c23a787777620e1f6fa2d58e
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-06-07 10:53:46 -0700

    added validation middleware

--- routers/projects.js
+++ routers/projects.js
@@ -1,76 +1,50 @@
 const express = require("express");
-const actionsData = require("../data/helpers/actionModel")
-const projectsData = require("../data/helpers/projectModel")
+const actionsDB = require("../data/helpers/actionModel")
+const projectsDB = require("../data/helpers/projectModel")
 const router = express.Router();
 
+//middleware
 
-router.get("/", (req, res) => {
-    projectsData
-        .get()
-        .then(projects =>{
-            res.status(200).json({projects});
-        })
-        .catch(() => {
-            res.status(500).json({ error: "Failure to get data from projects."});
-        });
-});
+const validateActionId = async (req, res, next) => {
+    try {
+    const {project_id} = req.body;
+    const action = await actionsDB.get(project_id);
 
-router.get("/:id", (req, res) => {
+    action 
+    ? next()
+    : res.status(404).json({message: "invalid id"});  
+    } catch(err) {
+        res.status(400).json({message: "missing action id"});
+    }
+}
+
+const validateProjectId = async (req, res, next) => {
+    try {
     const {id} = req.params;
-    projectsData
-        .get(id)
-        .then(projects => {
-            res.status(200).json({projects})
-        })
-        .catch(() => {
-            res.status(404).json({error: "Failure to get this project."});
-        });
-});
+    const project = await projectsDB.get(id);
 
-//name, description
-router.post("/", (req, res) => {
-    if(req.body.name && req.body.description) {
-        projectsData
-            .insert(req.body)
-            .then(response => {
-                res.status(201).json({response})
-            })
-            .catch(() => {
-                res.status(500).json({error: "Failure to post action."});
-            });
-    } else {
-        res.status(404).json({error: "Required parameters are missing."});
+    project 
+    ? next()
+    : res.status(404).json({message: "invalid id"});  
+    } catch(err) {
+        res.status(400).json({message: "missing user id"});
     }
-});
+}
 
-router.put("/:id", (req, res) => {
-    if(req.body.name && req.body.description) {
-        projectsData
-            .update(req.params.id, req.body)
-            .then(project => {
-                res.status(200).json({project})
-            })
-            .catch(() => {
-                res.status(500).json({error: "Failure to update action."});
-            });
-    } else {
-        res.status(404).json({error: "Required parameters are missing."});
-    }
-});
+const validateActionBody = (req, res, next) => {
+    const {project_id, description, notes} = req.body;
 
-router.delete("/:id", (req, res) => {
-    if(req.params.id){
-        projectsData
-            .remove(req.params.id)
-            .then(response => {
-                res.status(200).json({response})
-            })
-            .catch(() => {
-                res.status(500).json({error: "Failure to delete action."})
-            })
-    } else {
-        res.status(404).json({error: "Required id is missing."});
-    }
-})
+    project_id && description && notes
+    ? next()
+    : res.status(400).json({message: "missing required fields for actions"})
+} 
 
-module.exports = router;
+const validateProjectBody = (req, res, next) => {
+    const { name, description } = req.body;
+
+    name 
+    ? description
+    ? next()
+    : res.status(400).json({message: "missing description"})
+    : res.status(400).json({message: "missing name"});
+};
