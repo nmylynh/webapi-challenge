c9c7f495c33f10672ef40ebed9a90a1f0be39b58
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-06-07 11:04:44 -0700

    added crud endpoints for projects

--- routers/projects.js
+++ routers/projects.js
@@ -48,3 +48,87 @@ const validateProjectBody = (req, res, next) => {
     : res.status(400).json({message: "missing description"})
     : res.status(400).json({message: "missing name"});
 };
+
+//endpoints
+
+router.get('/', async (req, res) => {
+    try {
+        const projects = await projectsDB.get();
+
+        res.status(200).json(projects);
+    } catch(err) {
+        res.status(500).json({success: false, err});
+    }
+});
+
+
+router.get('/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const project = await projectsDB.get(id);
+
+        res.status(200).json(project);
+    } catch(err) {
+        res.status(500).json({success: false, err});
+    }
+}); 
+
+router.get('/:id/actions', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const project = await projectsDB.getProjectActions(id);
+
+        res.status(200).json(project);
+    } catch (err) {
+        res.status(500).json({success: false, err});
+    }
+});
+
+router.post('/', validateProjectBody, async (req, res) => {
+    try {
+        const newProject = await projectsDB.insert(req.body);
+
+        res.status(201).json(newProject);
+    } catch(err) {
+        res.status(500).json({success: false, err});
+    }
+});
+
+router.post('/:id/actions', validateActionId, validateActionBody, async (req, res) => {
+    try {
+        const newAction = await actionsDB.insert(req.body);
+
+        res.status(201).json(newAction);
+    } catch(err) {
+        res.status(500).json({success: false, err});
+    }
+});
+
+router.put('/:id', validateProjectId, validateProjectBody, async (req, res) => {
+    try {
+        const {id} = req.params;
+        const updateProject = await projectsDB.update(id, req.body);
+
+        updateProject
+        ? res.status(200).json({message: "successfully updated project"})
+        : res.status(404).end()
+    } catch(err) {
+        res.status(500).json({success: false, err});
+    }
+});
+
+router.delete('/:id', validateProjectId, async (req, res) => {
+    try {
+        const {id} = req.params;
+        const success = await projectsDB.remove(id);
+
+        success ?
+         res.status(204).end() : res.status(404).end();
+    }  catch(err) {
+         res.status(500).json({success: false, err});
+    }
+});
+
+
+
+module.exports = router;
